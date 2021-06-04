b99d80b104a55e2734e3891bcc30048721e0fa1d
Rens - Thuis <rens@thedarecompany.com>
2021-06-04 14:28:52 +0200

    Added lobby privating

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,14 @@ function getLobbyTable() {
     var table_data = "";
 
     table_data += '<table class="table"><thead><tr>';
-    table_data += '<th>Lobby ID</th><th>Lobby name</th><th>Join</th>';
+    table_data += '<th>Lobby ID</th><th>Lobby name</th><th>Join</th><th>Availability</th>';
     table_data += '</tr></thead><tbody>';
     for(i = 0; i < data.length; i++) {
         table_data += '<tr>';
         table_data += '<td>' + data[i]['id'] + '</td>';
         table_data += '<td>' + data[i]['name'] + '</td>';
         table_data += '<td><a href="joinLobby.html?lobby_id=' + data[i]['id'] +'">Join</a></td>';
+        table_data += '<td>' + (data[i]['availability'] == 1 ? '<i class="fas fa-lock-open"></i>' : '<i class="fas fa-lock"></i>') + '</td>';
         table_data += '</tr>';
     }
     table_data += '</tbody></table>';
